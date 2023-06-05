9a2c0bc3bc3bcd1f824f8524ed743d65c42fda42
JvVenna <joaocovinha12@gmail.com>
2023-06-04 21:21:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import './style.css';
+import api from './services/api.js';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha o campo em branco!")
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    }
+    catch {
+      alert("Error")
+      setInput("")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text"
+          placeholder="digite seu CEP..."
+          value={input}
+
+        /*salvar no input*/
+         /*propriedade*/ onChange={(e /*nome evento*/) => setInput(e.target.value/*valor digitado pelo usuario*/)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          Procurar
+        </button>
+      </div>
+
+      {/* acessando a useState Cep */
+       Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complemento:{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+        </main>
+      )}
+
+
     </div>
   );
 }
